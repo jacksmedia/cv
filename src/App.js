028f3026af6a9c13283f05c34bab2bd3ba9423a0
jacksmedia <alex@j4cks.com>
2023-09-07 19:54:27 -0700

    :art: glitched v1.0

--- src/App.js
+++ src/App.js
@@ -3,31 +3,44 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Alexander Jacks</h1>
-        <h2>
-          <em>
-            <a
-              className="App-link"
-              href="https://twitter.com/xJ4cks"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              My Twitter
-            </a>
-          </em>
-        </h2>
-        <p>
-          This is my public landing page.
-        </p>
+      <header className="App-header lines">
+      <div class="main">
+
+        <div class="time"><span id="time"></span></div>
+        <div class="text">
+          <h1 class="vhs">Alexander Jacks</h1>
+        </div>
+        <div class="text">
         <a
           className="App-link"
           href="https://stash23.s3.us-west-2.amazonaws.com/Resume23Q3.pdf"
           target="_blank"
           rel="noopener noreferrer"
         >
-          View my Resume
+          <h2>View my Resume</h2>
         </a>
+        </div>
+        <div class="text">
+          <p>
+            This is my public landing page.
+          </p>
+        </div>
+        <div class="text">
+          <p>
+            <em>
+              <a
+                className="App-link"
+                href="https://twitter.com/xJ4cks"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                My Twitter
+              </a>
+            </em>
+          </p>
+        </div>
+        <div class="counter">REC <label id="minutes">00</label>:<label id="seconds">00</label></div>
+        </div>
       </header>
     </div>
   );
